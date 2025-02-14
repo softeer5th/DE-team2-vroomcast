66a0c83b044baa8fb7e78ba7f0fbc9f30e2631cb
reudekx <reudekx@gmail.com>
2025-02-14 18:33:44 +0900

    feat: add extract-combine workflow using airflow

--- mwaa/bucket/dags/workflow.py
+++ mwaa/bucket/dags/workflow.py
@@ -1,70 +1,98 @@
-from airflow import DAG
-from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+import json
 from datetime import datetime, timedelta
 
-import os
-import json
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.amazon.aws.operators.lambda_function import \
+    LambdaInvokeFunctionOperator
+from airflow.utils.file import find_path_from_directory
 
-extract_bucket = os.environ.get('MWAA_S3_EXTRACT_BUCKET')
-transform_bucket = os.environ.get('MWAA_S3_TRANSFORM_BUCKET')
+# 환경 변수는 Variable로 관리
+EXTRACT_BUCKET = Variable.get("MWAA_S3_EXTRACT_BUCKET")
+COMBINE_BUCKET = Variable.get("MWAA_S3_COMBINE_BUCKET")
 
-def get_keywords():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    json_path = os.path.join(current_dir, 'keywords.json')
-    with open(json_path, 'r') as f:
-        return json.load(f)
-    
-def get_community():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    json_path = os.path.join(current_dir, 'community.json')
-    with open(json_path, 'r') as f:
-        return json.load(f)
 
-keywords = get_keywords()
-community = get_community()
+def load_config(filename: str) -> dict:
+    """설정 파일을 로드하는 함수"""
+    try:
+        config_path = find_path_from_directory(f"../configs/{filename}", "dags")
+        with open(config_path, "r") as f:
+            return json.load(f)
+    except Exception as e:
+        raise Exception(f"Failed to load config file {filename}: {str(e)}")
+
+
+# 설정 파일 로드
+CARS = {item["car_id"]: item["keywords"] for item in load_config("car.json")}
+COMMUNITIES = load_config("community.json")
 
 default_args = {
-    'owner': 'vroomcast', # 작성자
-    'depends_on_past': False, # 이전 실행이 성공했는지 확인
-    'start_date': datetime(2024, 2, 10), # workflow 시작 날짜
-    'email_on_failure': True, # 실패 시 이메일 발송
-    'email_on_retry': True, # 재시도 시 이메일 발송
-    'retries': 10, # 실패 시 재시도 횟수
-    'retry_delay': timedelta(minutes=10) # 재시도 간격
+    "owner": "vroomcast",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 2, 10),
+    "email_on_failure": True,
+    "email_on_retry": True,
+    "retries": 10,
+    "retry_delay": timedelta(minutes=5),
 }
 
-# DAG 정의
-dag = DAG(
-    'vroomcast_workflow', # DAG 이름
-    default_args=default_args, # 기본 인수
-    description='ETL workflow for Vroomcast', # DAG 설명
-    schedule_interval=timedelta(days=1), # DAG 실행 주기
-    catchup=False # 과거 DAG 실행 여부 (backfill)
-)
+with DAG(
+    "vroomcast_workflow",
+    default_args=default_args,
+    description="Data pipeline workflow for Vroomcast",
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+) as dag:
 
-def create_extract_task(community: str, car_id: str, keyword: str, date: str) -> LambdaInvokeFunctionOperator:
-    """Extract 태스크를 생성하는 함수"""
-    return LambdaInvokeFunctionOperator(
-        task_id=f'extract_{community}_{car_id}_{keyword}',
-        function_name='vroomcast-lambda-extract-{community}',
-        payload={
-            "bucket": extract_bucket,
-            "keywords": [keyword],
-            "car_id": car_id,
-            "date": date
-        },
-        dag=dag
-    )
+    def create_extract_task(
+        community: str, car_id: str, keyword: str, date: str
+    ) -> LambdaInvokeFunctionOperator:
+        """Extract 태스크를 생성하는 함수"""
+        return LambdaInvokeFunctionOperator(
+            task_id=f"extract_{car_id}_{keyword}_{date}_{community}",
+            function_name=f"vroomcast-lambda-extract-{community}",
+            payload=json.dumps(
+                {
+                    "bucket": EXTRACT_BUCKET,
+                    "car_id": car_id,
+                    "keywords": [keyword],
+                    "date": date,
+                }
+            ),
+            dag=dag,
+        )
 
-def create_transform_task(community: str, car_id: str, date: str) -> LambdaInvokeFunctionOperator:
-    """Transform 태스크를 생성하는 함수"""
-    return LambdaInvokeFunctionOperator(
-        task_id=f'transform_{community}_{car_id}',
-        function_name='vroomcast-lambda-transform-{community}',
-        payload={
-            "bucket": transform_bucket,
-            "car_id": car_id,
-            "date": date
-        },
-        dag=dag
-    )
+    def create_combine_task(car_id: str, date: str) -> LambdaInvokeFunctionOperator:
+        """Combine 태스크를 생성하는 함수"""
+        return LambdaInvokeFunctionOperator(
+            task_id=f"combine_{car_id}_{date}",
+            function_name=f"vroomcast-lambda-combine",
+            payload=json.dumps(
+                {"bucket": COMBINE_BUCKET, "car_id": car_id, "date": date}
+            ),
+            dag=dag,
+        )
+
+    # DAG 내부에서 테스크 생성
+    extract_tasks = []
+    combine_tasks = []
+
+    # 각 커뮤니티와 차량 조합에 대해 태스크 생성
+    for car_info in CARS.items():
+        car_id = car_info[0]
+        keywords = car_info[1]
+
+        for community in COMMUNITIES:
+            # Extract 태스크 생성
+            car_extract_tasks = [
+                create_extract_task(community, car_id, keyword, "{{ ds }}")
+                for keyword in keywords
+            ]
+            extract_tasks.extend(car_extract_tasks)
+
+        # Combine 태스크 생성
+        combine_task = create_combine_task(community, car_id, "{{ ds }}")
+        combine_tasks.append(combine_task)
+
+        # 하나의 자동차에 대한 Extract 태스크가 모두 완료되면 Combine 태스크 실행
+        car_extract_tasks >> combine_task
