0284729dda99de6a976ee8b8aa64ac4ea9316333
reudekx <reudekx@gmail.com>
2025-02-16 17:24:41 +0900

    feat: perform initial local testing and update airflow code

--- mwaa/bucket/dags/workflow.py
+++ mwaa/bucket/dags/workflow.py
@@ -5,18 +5,22 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.amazon.aws.operators.lambda_function import \
     LambdaInvokeFunctionOperator
-from airflow.utils.file import find_path_from_directory
 
-# 환경 변수는 Variable로 관리
-EXTRACT_BUCKET = Variable.get("MWAA_S3_EXTRACT_BUCKET")
-COMBINE_BUCKET = Variable.get("MWAA_S3_COMBINE_BUCKET")
+
+import os
+
+
+# AIRFLOW_VAR_로 시작하는 환경 변수는 자동으로 Airflow Variable이 됨
+S3_BUCKET = Variable.get("S3_BUCKET")
+
+# 설정 파일 경로
+CONFIG_PATH = os.path.join(os.getenv("AIRFLOW_HOME"), "configs")
 
 
 def load_config(filename: str) -> dict:
     """설정 파일을 로드하는 함수"""
     try:
-        config_path = find_path_from_directory(f"../configs/{filename}", "dags")
-        with open(config_path, "r") as f:
+        with open(f"{CONFIG_PATH}/{filename}", "r") as f:
             return json.load(f)
     except Exception as e:
         raise Exception(f"Failed to load config file {filename}: {str(e)}")
@@ -29,7 +33,7 @@ COMMUNITIES = load_config("community.json")
 default_args = {
     "owner": "vroomcast",
     "depends_on_past": False,
-    "start_date": datetime(2024, 2, 10),
+    "start_date": datetime(2024, 2, 10, 2, 0, 0),
     "email_on_failure": True,
     "email_on_retry": True,
     "retries": 10,
@@ -45,30 +49,30 @@ with DAG(
 ) as dag:
 
     def create_extract_task(
-        community: str, car_id: str, keyword: str, date: str
+        community: str, car_id: str, keywords: list[str]
     ) -> LambdaInvokeFunctionOperator:
         """Extract 태스크를 생성하는 함수"""
         return LambdaInvokeFunctionOperator(
-            task_id=f"extract_{car_id}_{keyword}_{date}_{community}",
+            task_id=f"extract_{car_id}_{community}",
             function_name=f"vroomcast-lambda-extract-{community}",
             payload=json.dumps(
                 {
-                    "bucket": EXTRACT_BUCKET,
+                    "bucket": S3_BUCKET,
                     "car_id": car_id,
-                    "keywords": [keyword],
-                    "date": date,
+                    "keywords": keywords,
+                    "date": "{{ ds }}",
                 }
             ),
             dag=dag,
         )
 
-    def create_combine_task(car_id: str, date: str) -> LambdaInvokeFunctionOperator:
+    def create_combine_task(car_id: str) -> LambdaInvokeFunctionOperator:
         """Combine 태스크를 생성하는 함수"""
         return LambdaInvokeFunctionOperator(
-            task_id=f"combine_{car_id}_{date}",
+            task_id=f"combine_{car_id}",
             function_name=f"vroomcast-lambda-combine",
             payload=json.dumps(
-                {"bucket": COMBINE_BUCKET, "car_id": car_id, "date": date}
+                {"bucket": S3_BUCKET, "car_id": car_id, "date": "{{ ds }}"}
             ),
             dag=dag,
         )
@@ -77,21 +81,21 @@ with DAG(
     extract_tasks = []
     combine_tasks = []
 
-    # 각 커뮤니티와 차량 조합에 대해 태스크 생성
+    # 각 차종에 대해 Extract 태스크와 Combine 태스크 생성
     for car_info in CARS.items():
         car_id = car_info[0]
         keywords = car_info[1]
 
-        for community in COMMUNITIES:
-            # Extract 태스크 생성
-            car_extract_tasks = [
-                create_extract_task(community, car_id, keyword, "{{ ds }}")
-                for keyword in keywords
-            ]
-            extract_tasks.extend(car_extract_tasks)
+        # 특정 차종에 대해 커뮤니티마다 수행되는 Extract 태스크 생성
+        car_extract_tasks = [
+            create_extract_task(community, car_id, keywords)
+            for community in COMMUNITIES
+        ]
+
+        extract_tasks.extend(car_extract_tasks)
 
         # Combine 태스크 생성
-        combine_task = create_combine_task(community, car_id, "{{ ds }}")
+        combine_task = create_combine_task(car_id)
         combine_tasks.append(combine_task)
 
         # 하나의 자동차에 대한 Extract 태스크가 모두 완료되면 Combine 태스크 실행
