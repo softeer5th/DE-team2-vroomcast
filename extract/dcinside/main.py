32354301575922b5e42feb1926b3fa1f13d39b0e
산야 <vs501kr@gmail.com>
2025-02-20 19:19:38 +0900

    .

--- extract/dcinside/main.py
+++ extract/dcinside/main.py
@@ -109,25 +109,25 @@ class DC_crawler:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--disable-dev-tools")
-        chrome_options.add_argument("--no-zygote")
+        # chrome_options.add_argument("--no-zygote")
         chrome_options.add_argument("--single-process")
         chrome_options.add_argument(f"--user-data-dir={mkdtemp()}")
-        chrome_options.add_argument(f"--data-path={mkdtemp()}")
-        chrome_options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-        chrome_options.add_argument("--remote-debugging-pipe")
-        chrome_options.add_argument("--verbose")
-        chrome_options.add_argument("--log-path=/tmp")
+        # chrome_options.add_argument(f"--data-path={mkdtemp()}")
+        # chrome_options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+        # chrome_options.add_argument("--remote-debugging-pipe")
+        # chrome_options.add_argument("--verbose")
+        # chrome_options.add_argument("--log-path=/tmp")
         chrome_options.binary_location = "/opt/chrome/chrome-linux64/chrome"
-        prefs = {
-            "profile.managed_default_content_settings.images": 2,  # 이미지 비활성화
-            "profile.managed_default_content_settings.ads": 2,     # 광고 비활성화
-            "profile.managed_default_content_settings.media": 2    # 비디오, 오디오 비활성화
-        }
-        chrome_options.add_experimental_option("prefs", prefs)
+        # prefs = {
+        #     "profile.managed_default_content_settings.images": 2,  # 이미지 비활성화
+        #     "profile.managed_default_content_settings.ads": 2,     # 광고 비활성화
+        #     "profile.managed_default_content_settings.media": 2    # 비디오, 오디오 비활성화
+        # }
+        # chrome_options.add_experimental_option("prefs", prefs)
 
         service = Service(
             executable_path="/opt/chrome-driver/chromedriver-linux64/chromedriver",
-            service_log_path="/tmp/chromedriver.log"
+            # service_log_path="/tmp/chromedriver.log"
         )
         driver = Chrome(
             service=service, # 도커 환경에서 사용시 주석 해제하세요.
