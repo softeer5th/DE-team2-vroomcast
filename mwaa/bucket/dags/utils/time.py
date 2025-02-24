3723324306197e9c84a88059882d4b251906d529
reudekx <reudekx@gmail.com>
2025-02-25 00:05:27 +0900

    docs: add comments

--- mwaa/bucket/dags/utils/time.py
+++ mwaa/bucket/dags/utils/time.py
@@ -11,6 +11,16 @@ from utils.xcom import create_push_to_xcom_task, pull_from_xcom
 def create_push_time_info_task(
     dag: DAG, date: str, time: str, batch: int
 ) -> PythonOperator:
+    """
+    Logical Datetime 정보를 XCom에 저장하는 Task를 생성합니다.
+    Args:
+        dag (DAG): Airflow DAG
+        date (str): 날짜
+        time (str): 시간
+        batch (int): 배치
+    Returns:
+        PythonOperator: Task
+    """
     return create_push_to_xcom_task(
         dag,
         "time_info",
@@ -23,7 +33,13 @@ def create_push_time_info_task(
 
 
 def create_push_start_time_task(dag: DAG) -> PythonOperator:
-
+    """
+    작업 시작 시간을 XCom에 저장하는 Task를 생성합니다.
+    Args:
+        dag (DAG): Airflow DAG
+    Returns:
+        PythonOperator: Task
+    """
     KST = timezone("Asia/Seoul")
     current_datetime = datetime.now(KST)
 
@@ -38,6 +54,13 @@ def create_push_start_time_task(dag: DAG) -> PythonOperator:
 
 
 def pull_time_info(**context) -> dict[str, str | int]:
+    """
+    Logical Datetime 정보를 XCom에서 가져옵니다.
+    Args:
+        **context: Airflow Context
+    Returns:
+        dict[str, str | int]: 날짜, 시간, 배치 정보
+    """
     return {
         "date": pull_from_xcom("time_info", "date", **context),
         "time": pull_from_xcom("time_info", "time", **context),
@@ -48,6 +71,16 @@ def pull_time_info(**context) -> dict[str, str | int]:
 def get_time_diff(
     start_date: str, start_time: str, current_date: str, current_time: str
 ) -> str:
+    """
+    두 날짜와 시간 사이의 시간 차이를 계산합니다.
+    Args:
+        start_date (str): 시작 날짜
+        start_time (str): 시작 시간
+        current_date (str): 현재 날짜
+        current_time (str): 현재 시간
+    Returns:
+        str: 시간 차이 (HH:MM:SS)
+    """
     current_datetime = datetime.strptime(
         f"{current_date} {current_time}", "%Y-%m-%d %H:%M:%S"
     )
